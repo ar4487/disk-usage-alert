c2916f007228e6ebe711b81520a7d04ec466ea5a
root <root@arpitbhadoria9@gmail.com>
2025-08-04 09:34:42 +0530

    Initial commit: Disk usage alert script

---
+++ disk_alert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Disk Usage Alert Script for Linux
+
+THRESHOLD=80
+EMAIL='arpitbhadoria9@gmail.com'
+LOG_FILE="/tmp/disk_usage_alert.log"
+
+DISK_STATS=$(df -h / | awk 'NR==2 {print $5 " " $4 " " $6}')
+USAGE=$(echo $DISK_STATS | awk '{print $1}' | sed 's/%//g')
+AVAILABLE=$(echo $DISK_STATS | awk '{print $2}')
+MOUNTED_ON=$(echo $DISK_STATS | awk '{print $3}')
+TIMESTAMPS=$(date '+%Y-%m-%d %H:%M:%S')
+
+if [ "$USAGE" -ge "$THRESHOLD" ]; then
+	MESSAGE ="Disk usage alert on $(hostname) at $TIMESTAMPS.
+	Root filesystem is $USAGE% full ($AVAILABLE available on $MOUNTED_ON)."
+	echo "$MESSAGE" >> "$LOG_FILE"
+	echo "$MESSAGE" | mail -s "Disk Usage alert: $USAGE% full" "$EMAIL"
+else
+	echo "$TIMESTAMP -ok: $USAGE% used, $AVAILABLE available." >> "$LOG_FILE"
+fi
